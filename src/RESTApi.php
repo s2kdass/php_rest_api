a910eac7c854af08a2b6b8baa0ba7cd5211bed8b
Santhosh <s2kdass@gmail.com>
2019-03-24 09:00:49 +0530

    namespace changed

--- src/RESTApi.php
+++ src/RESTApi.php
@@ -1,7 +1,20 @@
 <?php
 
-namespace RESTApi;
+namespace s2kdass;
 
 class RESTApi{
 
+    public $statusCode = 200;
+
+    public function __construct() : self{
+
+    }
+
+    public function render(array $jsonArray = []) {
+        echo json_encode([
+            'statusCode' => $this->statusCode,
+            'data' => $jsonArray
+        ]);
+        exit;
+    }
 }
